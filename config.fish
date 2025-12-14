ea412af69b962411907fb3a582377009d34a3f5d
Matrix <rytchiemacharia@gmail.com>
2025-12-14 18:28:31 +0000

    initial commit

---
+++ config.fish
@@ -0,0 +1,53 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive # Commands to run in interactive sessions can go here
+
+    # No greeting
+    set fish_greeting
+
+    # Use starship
+    starship init fish | source
+    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
+
+    # Aliases
+    alias pamcan pacman
+    alias ls 'eza --icons'
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias q 'qs -c ii'
+    
+end
+
+# functions
+function sys_update
+    printf "=== Updating the system... ==="
+    sleep 3
+
+    sudo pacman -Sy 
+
+    printf "=== Finished Updating the System! ==="
+end
+
+function sys_upgrade
+    printf "=== Ugrading the system... ==="
+    sleep 3
+
+    sudo pacman -Syu 
+
+    printf "=== Finished Upgrading the System! ==="
+end
+
+alias update=sys_update
+alias upgrade=sys_upgrade
+alias search='sudo pacman -Ss'
+alias searchy='yay -Ss'
+alias d=docker
+alias drit='docker run -it'
+alias weather='curl http://wttr.in/kenya'
