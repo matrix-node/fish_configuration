083fdadeb31b1dab022c45e462d45f1b261adb63
Matrix <rytchiemacharia@gmail.com>
2025-12-15 19:36:36 +0000

    added the extract & Quick backup function

--- config.fish
+++ config.fish
@@ -33,6 +33,7 @@ function sys_update
     sudo pacman -Sy
     sudo yay -Sy
 
+    sleep 2
     printf "=== Finished Updating the System! ==="
 end
 
@@ -43,8 +44,46 @@ function sys_upgrade
     sudo pacman -Syu
     sudo yay -Syu
 
+    sleep 2
     printf "=== Finished Upgrading the System! ==="
 end
+
+# Extract any archive
+function extract
+    if test -f $argv[1]
+        switch $argv[1]
+            case "*.tar.bz2"
+                tar xjf $argv[1]
+            case "*.tar.gz"
+                tar xzf $argv[1]
+            case "*.bz2"
+                bunzip2 $argv[1]
+            case "*.rar"
+                unrar x $argv[1]
+            case "*.gz"
+                gunzip $argv[1]
+            case "*.tar"
+                tar xf $argv[1]
+            case "*.zip"
+                unzip $argv[1]
+            case "*.Z"
+                uncompress $argv[1]
+            case "*.7z"
+                7z x $argv[1]
+            case "*"
+                echo "'$argv[1]' cannot be extracted"
+        end
+    else
+        echo "'$argv[1]' is not a valid file"
+    end
+end
+
+# Quick backup
+function backup
+    cp $argv[1] $argv[1].backup-(date +%Y%m%d-%H%M%S)
+end
+
+# Aliases for basic commands
 
 alias update=sys_update
 alias upgrade=sys_upgrade
